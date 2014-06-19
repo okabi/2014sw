9a75c7030b3b0107f69be6d3e1e80c4c91ef4124
okabi <a0120051@ws15.(none)>
2014-06-19 16:17:41 +0900

    課題8途中まで(詳しくはREADME)

--- generate_assemble.rb
+++ generate_assemble.rb
@@ -1,14 +1,25 @@
 # -*- coding: utf-8 -*-
+# 書き出すアセンブリファイル名
 $filename = ARGV[0]
 if $filename == nil
   $filename = "test.asm"
 end
 $file = open($filename, "w")
+
+# 宣言された関数名のリスト
 $functions = []
+
+# 宣言されたローカル変数名(レベル含む)のリスト
 $local_vars = {}
+
+# 関数culcNLocal用
 $n_local = 0
 
+
+
+# アセンブリコードを生成する
 def generateAssemble(tree)
+  # アセンブリコードを1行書き出す
   def putsFile(a, b=nil, c=nil, d=nil)
     $file.print("#{a}") if a != nil
     $file.print("\t#{b}") if b != nil
@@ -17,6 +28,9 @@ def generateAssemble(tree)
     $file.puts("")
   end
 
+
+  # そのノードから下に伸びるノード中で使われる相対番地の絶対値の最大値を求める
+  # 大域変数$n_localに書き出される
   def culcNLocal(node)
     for n in node
       if n[0] == "int"
@@ -31,28 +45,63 @@ def generateAssemble(tree)
     end
   end
 
+
+  # 数あるいは"#{name}:#{type}:level#{level}"を、コード形式に変換して返す
+  def leafToCode(leaf)
+    if leaf.instance_of?(String) == true
+      if $local_vars[leaf] != nil && $local_vars[leaf] >= 0
+        return "[ebp+#{$local_vars[leaf]}]"
+      else
+        return "[ebp#{$local_vars[leaf]}]"
+      end
+    else
+      return "#{leaf}"
+    end
+  end
+
+
+  # 木を掘り進んで順番にコードを生成する
   def digTree(parent_node, child_node, lvl=-1)
+    # 現在見ているノード(配列)が最も深いノードか調べる
+    leaf = true
+    for n in child_node
+      if n.instance_of?(String) == false
+        leaf = false
+        break
+      end
+    end
+
+    # ノードの先頭要素が文字列なら、再帰的にコード生成処理を行う
+    # 宣言文以外で葉だったなら、再帰末端用コードを生成する
     if child_node[0].instance_of?(String) == true
       case child_node[0]
       when "int"
+        # 関数宣言または変数宣言
+        # 関数 => ("int", "#{name}:#{type}:level#{level}")
+        # 変数 => ("int", "#{name}:#{type}:level#{level}(#{pos})")
         child_node[1] =~ /(.+):(.+):(.+)/
         name = $1
         type = $2
         level = $3
         pos = nil
         if level.index("level0") == nil
+          # パラメータかローカル変数
           child_node[1] =~ /(.+):(.+):level(.+)\((.+)\)/
           level = $3.to_i
           pos = $4.to_i
         else
+          # 大域関数か大域変数
           level =~ /level(.+)/
           level = $1.to_i
         end
         if level == 0
+          # 大域関数か大域変数のとき
           if type == "VAR"
+            # 大域変数のとき
             putsFile(nil, "GLOBAL", name)
-            putsFile(nil, "COMMON", name, 4)
+            putsFile(nil, "COMMON", "#{name} 4")
           elsif type == "FUN"
+            # 大域関数の時
             $functions.push(name)
             putsFile(nil, "GLOBAL", name)
             putsFile(name, "push", "ebp")           
@@ -67,14 +116,14 @@ def generateAssemble(tree)
             end
             $file.puts("; 関数#{name}の本体ここまで")
             
-            putsFile("Lret", "mov", "esp", "ebp")
+            putsFile("#{name}ret", "mov", "esp", "ebp")
             putsFile(nil, "pop", "ebp")
             putsFile(nil, "ret")
             $local_vars.clear
           end
         else
-          $local_vars["#{name}:level#{level}"] = pos
-          $file.puts("; local_vars: #{name}:level#{level} => #{pos}")
+          $local_vars["#{name}:#{type}:level#{level}"] = pos
+          $file.puts("; local_vars: #{name}:#{type}:level#{level} => #{pos}")
         end
       when "WHILE"
         $file.puts("; WHILEここから")
@@ -100,8 +149,78 @@ def generateAssemble(tree)
         if $functions.index(child_node[1]) == nil
           putsFile(nil, "EXTERN", child_node[1])          
         end
-          putsFile(nil, "call", child_node[1])                  
-      end
+        putsFile(nil, "call", child_node[1])
+        child_node[2].length.times do |i|
+          putsFile(nil, "pop", "ebx")
+        end
+      when "="
+        # 代入命令
+        $file.puts("; = ここから(#{child_node[1]}, #{child_node[2]})")
+        if child_node[2].instance_of?(String) == true || child_node[2].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", leafToCode(child_node[1]), leafToCode(child_node[2]))
+        else
+          digTree(child_node, child_node[2], lvl + 1)
+          putsFile(nil, "mov", leafToCode(child_node[1]), "eax")
+        end
+        $file.puts("; = ここまで(#{child_node[1]}, #{child_node[2]})")
+      when "+"
+        # 加算命令
+        $file.puts("; + ここから(#{child_node[1]}, #{child_node[2]})")
+        if child_node[2].instance_of?(String) == true || child_node[2].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", "eax", leafToCode(child_node[2]))          
+          putsFile(nil, "push", "eax")
+        else
+          digTree(child_node, child_node[2], lvl + 1)
+          putsFile(nil, "push", "eax")
+        end
+        if child_node[1].instance_of?(String) == true || child_node[1].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", "eax", leafToCode(child_node[1]))
+        else
+          digTree(child_node, child_node[1], lvl + 1)
+        end
+        putsFile(nil, "pop", "ebx")        
+        putsFile(nil, "add", "eax", "ebx")
+        $file.puts("; + ここまで(#{child_node[1]}, #{child_node[2]})")
+      when "-"
+        # 減算命令
+        $file.puts("; - ここから(#{child_node[1]}, #{child_node[2]})")
+        if child_node[2].instance_of?(String) == true || child_node[2].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", "eax", leafToCode(child_node[2]))          
+          putsFile(nil, "push", "eax")
+        else
+          digTree(child_node, child_node[2], lvl + 1)
+          putsFile(nil, "push", "eax")
+        end
+        if child_node[1].instance_of?(String) == true || child_node[1].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", "eax", leafToCode(child_node[1]))
+        else
+          digTree(child_node, child_node[1], lvl + 1)
+        end
+        putsFile(nil, "pop", "ebx")        
+        putsFile(nil, "sub", "eax", "ebx")
+        $file.puts("; - ここまで(#{child_node[1]}, #{child_node[2]})")
+      when "*"
+        # 乗算命令
+        $file.puts("; * ここから(#{child_node[1]}, #{child_node[2]})")
+        if child_node[2].instance_of?(String) == true || child_node[2].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", "eax", leafToCode(child_node[2]))          
+          putsFile(nil, "push", "eax")
+        else
+          digTree(child_node, child_node[2], lvl + 1)
+          putsFile(nil, "push", "eax")
+        end
+        if child_node[1].instance_of?(String) == true || child_node[1].instance_of?(Fixnum) == true
+          putsFile(nil, "mov", "eax", leafToCode(child_node[1]))
+        else
+          digTree(child_node, child_node[1], lvl + 1)
+        end
+        putsFile(nil, "pop", "ebx")        
+        putsFile(nil, "imul", "eax", "ebx")
+        $file.puts("; * ここまで(#{child_node[1]}, #{child_node[2]})")
+      when "/"
+        $file.puts("; / めんどい")
+      end    
+
     else
       if lvl < 0
         for grandchild in child_node
--- test.asm
+++ test.asm
@@ -1,20 +1,63 @@
 	GLOBAL	x
-	COMMON	x, 4
+	COMMON	x 4
 	GLOBAL	f
 f	push	ebp
 	mov	ebp, esp
 	sub	esp, 20
 ; 関数fの本体ここから
-; local_vars: x:level2 => -4
+; local_vars: x:VAR:level2 => -4
 ; WHILEここから
-; local_vars: x:level3 => -8
-; local_vars: y:level3 => -12
-; local_vars: x:level4 => -16
-; local_vars: z:level4 => -20
+; local_vars: x:VAR:level3 => -8
+; local_vars: y:VAR:level3 => -12
+; + ここから(x:VAR:level3, y:VAR:level3)
+	mov	eax, [ebp-12]
+	push	eax
+	mov	eax, [ebp-8]
+	pop	ebx
+	add	eax, ebx
+; + ここまで(x:VAR:level3, y:VAR:level3)
+; local_vars: x:VAR:level4 => -16
+; local_vars: z:VAR:level4 => -20
+; = ここから(x:VAR:level4, ["-", ["*", "x:VAR:level4", 3], ["/", "z:VAR:level4", "x:VAR:level4"]])
+; - ここから(["*", "x:VAR:level4", 3], ["/", "z:VAR:level4", "x:VAR:level4"])
+; / めんどい
+	push	eax
+; * ここから(x:VAR:level4, 3)
+	mov	eax, 3
+	push	eax
+	mov	eax, [ebp-16]
+	pop	ebx
+	imul	eax, ebx
+; * ここまで(x:VAR:level4, 3)
+	pop	ebx
+	sub	eax, ebx
+; - ここまで(["*", "x:VAR:level4", 3], ["/", "z:VAR:level4", "x:VAR:level4"])
+	mov	[ebp-16], eax
+; = ここまで(x:VAR:level4, ["-", ["*", "x:VAR:level4", 3], ["/", "z:VAR:level4", "x:VAR:level4"]])
 ; WHILEここまで
-; local_vars: w:level3 => -8
+; local_vars: w:VAR:level3 => -8
+; + ここから(["+", "x:VAR:level2", "y:VAR:level1"], w:VAR:level3)
+	mov	eax, [ebp-8]
+	push	eax
+; + ここから(x:VAR:level2, y:VAR:level1)
+	mov	eax, [ebp]
+	push	eax
+	mov	eax, [ebp-4]
+	pop	ebx
+	add	eax, ebx
+; + ここまで(x:VAR:level2, y:VAR:level1)
+	pop	ebx
+	add	eax, ebx
+; + ここまで(["+", "x:VAR:level2", "y:VAR:level1"], w:VAR:level3)
+; + ここから(x:VAR:level2, y:VAR:level1)
+	mov	eax, [ebp]
+	push	eax
+	mov	eax, [ebp-4]
+	pop	ebx
+	add	eax, ebx
+; + ここまで(x:VAR:level2, y:VAR:level1)
 ; 関数fの本体ここまで
-Lret	mov	esp, ebp
+fret	mov	esp, ebp
 	pop	ebp
 	ret
 	GLOBAL	g
@@ -22,12 +65,14 @@ g	push	ebp
 	mov	ebp, esp
 	sub	esp, 4
 ; 関数gの本体ここから
-; local_vars: z:level2 => -4
+; local_vars: z:VAR:level2 => -4
 	push	x:VAR:level0
 	push	x:VAR:level0
 	call	f
+	pop	ebx
+	pop	ebx
 	call	g
 ; 関数gの本体ここまで
-Lret	mov	esp, ebp
+gret	mov	esp, ebp
 	pop	ebp
 	ret
